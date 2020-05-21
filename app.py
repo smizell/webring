643d08a65f8ab8f9e33ccc57a4b92ebb9a04f32b
Stephen Mizell <smizell@gmail.com>
2020-05-20 23:06:58 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask import redirect
+import random
+
+app = Flask(__name__)
+
+sites = [
+    {
+        'id': 'smizell',
+        'url': 'https://smizell.com'
+    },
+]
+
+def next_site(site_id):
+    for index, site in enumerate(sites):
+        if site_id == site['id']:
+            if index == (len(sites) - 1):
+                return sites[0]
+            return sites[index + 1]
+    return random_site()
+
+def previous_site(site_id):
+    for index, site in enumerate(sites):
+        if site_id == site['id']:
+            if index == 0:
+                return sites[len(sites) - 1]
+            return sites[index - 1]
+    return random_site()
+
+def random_site():
+    return random.choice(sites)
+
+@app.route('/', methods=['GET'])
+@app.route('/random', methods=['GET'])
+def random_route():
+    return redirect(random_site()['url'])
+
+@app.route('/site/<string:site_id>/next', methods=['GET'])
+def next_route(site_id):
+    return redirect(next_site(site_id)['url'])
+
+@app.route('/site/<string:site_id>/previous', methods=['GET'])
+def previous_route(site_id):
+    return redirect(previous_site(site_id)['url'])
